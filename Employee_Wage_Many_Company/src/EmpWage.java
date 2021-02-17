8dd70e464aa5b58f30b835d1271b842101e31df4
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-02-17 15:58:28 +0530

    Added:Refactor Employee wage builder for multiple company

--- Employee_Wage_Many_Company/src/EmpWage.java
+++ Employee_Wage_Many_Company/src/EmpWage.java
@@ -2,71 +2,55 @@ public class EmpWage {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
 
- 	
- 	  // Variables
-    private final String company;
-    private final int empRatePerHr;
-    private final int noOfWorkingDays;
-    private final int maxHrsPeronth;
+    private int numOfCompany = 0;
+	private CompanyEmpWage[] companyEmpWageArray;
+	
+	public EmpWage() {
+		companyEmpWageArray = new CompanyEmpWage[5];
+	}
+	
+	private void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+		numOfCompany++;
+	}
+	
+	private void computeEmpWage() {
+		for(int i = 0;i < numOfCompany; i++) {
+			companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
+			System.out.println(companyEmpWageArray[i]);
+			System.out.println();
+		}
+	}
+	
+	public int computeEmpWage(CompanyEmpWage companyEmpWage ) {
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		while(totalEmpHrs <= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays) {
+			totalWorkingDays++;
+			int employeeCheck = (int)Math.floor(Math.random() * 10) % 3;
 
-    // Constructer
-    public EmpWage(String company, int empRatePerHr, int noOfWorkingDays, int maxHrsPeronth) {
-        this.company = company;
-        this.empRatePerHr = empRatePerHr;
-        this.noOfWorkingDays = noOfWorkingDays;
-        this.maxHrsPeronth = maxHrsPeronth;
-    }
-
-    // Compute Wage method
-    public void computeEmpWage() {
-        // Variables
-        int empHrs = 0;
-        int totalEmpWage = 0;
-        int totalWorkingDays = 0;
-        int totalEmpHrs = 0;
-
-        // Computation
-        while (totalEmpHrs <= maxHrsPeronth && totalWorkingDays < noOfWorkingDays) {
-            totalWorkingDays++;
-            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-            // Case Checking
-            switch (empCheck) {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
-            }// End Case
-            totalEmpHrs += empHrs;
-            System.out.println("Day: " + totalWorkingDays + "\tEmp Hr: " + empHrs);
-        } // End While
-        totalEmpWage = totalEmpHrs * empRatePerHr;
-        System.out.println("Total Employee Wage for company " + company + ": " + totalEmpWage + "\n");
-
-    } // End computeEmpWage
-
-    public static void main(String[] args) throws Exception {
-
-        // Welcome message
-        System.out.println("Welcome to Employee Wedge Builder...");
-      
-
-        // Objects
-        
-        EmpWage dMart = new EmpWage("Dmart", 20, 2, 10);
-        EmpWage reliance = new EmpWage("Reliance", 10, 4, 20);
-        EmpWage amazon = new EmpWage("Amazon", 20, 2, 10);
-
-        // Method calls
-        dMart.computeEmpWage();
-        reliance.computeEmpWage();
-        amazon.computeEmpWage();
-
-    }// End main
+			switch (employeeCheck) {
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;	
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+			}	
+			totalEmpHrs += empHrs;
+			System.out.println("Day#: "+ totalWorkingDays +"Emp Hr: "+empHrs);
+		}
+		return totalEmpHrs * companyEmpWage.empRatePerHour;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Employee Wage Computation Problem");
+		EmpWage empWageBuilder = new EmpWage();
+		empWageBuilder.addCompanyEmpWage("dMart", 20, 2, 10);
+		empWageBuilder.addCompanyEmpWage("jioMart", 10, 4, 20);
+		empWageBuilder.computeEmpWage();
+	}
 }// End class EmpWageBuilder
  
   	
